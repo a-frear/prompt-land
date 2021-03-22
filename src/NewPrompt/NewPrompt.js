042b475d86e0cfcd79663eca82b02f3196b53e44
a_frear <amy.frear@gmail.com>
2021-03-22 11:36:02 -0400

    Update NewPrompt.js

--- src/NewPrompt/NewPrompt.js
+++ src/NewPrompt/NewPrompt.js
@@ -41,7 +41,7 @@ const NewPrompt = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     function goHome(){
-      history.push('/')
+      history.go('/')
     } 
     const token = await getAccessTokenSilently();
     const newPrompt = {
