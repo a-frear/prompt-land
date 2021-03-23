ddee14d0c3851bcdbcba4cd570bbe1e340557279
a_frear <amy.frear@gmail.com>
2021-03-23 18:46:03 -0400

    Update NewPrompt.js

--- src/NewPrompt/NewPrompt.js
+++ src/NewPrompt/NewPrompt.js
@@ -41,7 +41,7 @@ const NewPrompt = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     function goHome() {
-      history.push("/");
+      history.push("/feed");
     }
     const token = await getAccessTokenSilently();
     const newPrompt = {
