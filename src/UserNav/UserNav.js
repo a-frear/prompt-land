923ccbfd176a0dde93b1adf95de4423059e2b455
Amy Frear <amy.frear@gmail.com>
2021-12-10 15:26:40 -0500

    Remove setIsActive

--- src/UserNav/UserNav.js
+++ src/UserNav/UserNav.js
@@ -10,7 +10,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const UserNav = () => {
   const { isAuthenticated } = useAuth0();
-  const [isActive, setIsActive] = useState();
+  const [isActive] = useState();
 
   return (
     isAuthenticated && (
